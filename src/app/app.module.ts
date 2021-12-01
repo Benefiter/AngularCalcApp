9e41cbf9d8c314ea45d890981846a70411b545da
benhmwilfred <benhmwilfred@gmail.com>
2021-12-01 00:39:47 -0700

    Relocator event driven calculator under components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CalcheaderComponent } from './components/calcheader/calcheader.component';
-import { CalcbuttonComponent } from './components/calcbutton/calcbutton.component';
-import { CalculatorComponent } from './components/calculator/calculator.component';
-import { CalcoperandComponent } from './components/calcoperand/calcoperand.component';
+import { CalcbuttonComponent } from './components/EventingCalculator/calcbutton/calcbutton.component';
+import { CalculatorComponent } from './components/EventingCalculator/calculator/calculator.component';
+import { CalcoperandComponent } from './components/EventingCalculator/calcoperand/calcoperand.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { IconComponent } from './components/icon/icon.component';
-import { IconmenuComponent } from './components/iconmenu/iconmenu.component';
+import { IconComponent } from './components/EventingCalculator/icon/icon.component';
+import { IconmenuComponent } from './components/EventingCalculator/iconmenu/iconmenu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { CalcheaderComponent } from './components/EventingCalculator/calcheader/calcheader.component';
 
 @NgModule({
   declarations: [
